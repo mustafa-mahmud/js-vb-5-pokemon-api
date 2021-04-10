4076086cadedecc2df6b509951911d5bbfd2b7f7
mustafa-mahmud <mithuweb000@gmail.com>
2021-04-10 20:57:57 +0600

    end project

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+'use strict';
+
+const container = document.querySelector('.container');
+const cards = 200;
+
 const colors = {
   fire: '#FDDFDF',
   grass: '#DEFDE0',
@@ -15,4 +20,39 @@ const colors = {
   normal: '#F5F5F5',
 };
 
-//https://pokeres.bastionbot.org/images/pokemon/5.png
+function init() {
+  for (let i = 1; i <= cards; i++) {
+    fetchData(i);
+  }
+}
+
+init();
+
+async function fetchData(id) {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+
+  createCards(data);
+}
+
+function createCards(data) {
+  const div = document.createElement('div');
+  div.classList.add('pokemon');
+
+  const innerEl = `
+			<img src="https://pokeres.bastionbot.org/images/pokemon/${
+        data.id
+      }.png" alt="pokemon" />
+				<div class="info">
+					<p>Id: #${data.id.toString().padStart(3, '0')}</p>
+					<p>Name: ${data.name}</p>
+					<p>Power: ${data.types[0].type.name}</p>
+			</div>
+	`;
+
+  div.innerHTML = innerEl;
+
+  container.appendChild(div);
+  console.log(data);
+}
